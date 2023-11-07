0942fc0437dace0facd7f0db641532880c6e80d9
VictorJunqueira1 <victorjunqueira21@gmail.com>
2023-11-07 13:26:51 -0300

    feat: Automação do botão adicionar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { TodoItem } from "@/types/TodoItem";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
   const [itemInput, setItemInput] = useState('')
@@ -11,10 +11,10 @@ export const Home = () => {
   ])
 
   const handleAddButton = () => {
-    if(itemInput.trim() === '') return;
+    if (itemInput.trim() === '') return;
     setList(
       [...list,
-        { label: itemInput, checked: false }
+      { label: itemInput, checked: false }
       ]
     );
     setItemInput('');
@@ -23,21 +23,24 @@ export const Home = () => {
   const handleDelete = (index: number) => {
     setList(list.filter((item, key) => key !== index));
   }
-  
+
   return (
     <div className="h-screen w-full flex items-center justify-center flex-col gap-6">
       <h1 className="font-bold text-2xl">Lista de Tarefas</h1>
 
       <div>
         <label className="border-2 border-none bg-slate-600 rounded-lg p-6">
-          <input type="text" 
-            className="p-4 py-2 rounded-sm" 
-            placeholder="O que deseja fazer?" 
+          <input type="text"
+            className="p-4 py-2 rounded-sm"
+            placeholder="O que deseja fazer?"
             value={itemInput}
             onChange={e => setItemInput(e.target.value)}
+            onKeyUp={(e) => {
+              if(e.key === "Enter") handleAddButton()
+            }}
           />
-          <button 
-          className="text-white m-2 p-2" onClick={handleAddButton}>Adicionar
+          <button
+            className="text-white m-2 p-2" onClick={handleAddButton}>Adicionar
           </button>
         </label>
       </div>
@@ -52,6 +55,7 @@ export const Home = () => {
         }
         </ul>
       </div>
+
     </div>
   )
 }
